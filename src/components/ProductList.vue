8d0b39e03448075ce632ccc2f1133b8a909bc798
takuya-nakayasu <l08084.1989@gmail.com>
2023-03-21 19:43:54 +0900

    fix

---
+++ src/components/ProductList.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { getProducts } from '../api/shop'
+
+const products = ref([])
+onMounted(() => {
+    getProducts((data: any) => (products.value = data))
+})
+</script>
+
+<template></template>
