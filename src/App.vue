63c665f2ef6677e823c6cb76e6092eae6f11b194
takuya-nakayasu <l08084.1989@gmail.com>
2023-03-21 23:44:38 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,18 @@ import ProductList from './components/ProductList.vue';
 </script>
 
 <template>
-  <ProductList />
+  <div id="product">
+    <h1>Pinia入門(カート)</h1>
+    <hr />
+    <h2>商品一覧</h2>
+    <ProductList />
+  </div>
 </template>
+
+<style>
+#product {
+  font-family: Arial, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
